582290da28a48d706b0f49cafe3ee43027986fcd
BichoPalillo <162630409+BichoPalillo@users.noreply.github.com>
2025-03-04 21:27:23 +0100

    Participa1
    A falta de codificar el grid pane

--- CiclismoWindows/src/main/java/controladores/participacionesController.java
+++ CiclismoWindows/src/main/java/controladores/participacionesController.java
@@ -1,4 +1,38 @@
 package controladores;
 
-public class participacionesController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class participacionesController implements Initializable {
+
+    @FXML
+    private GridPane P_grid;
+
+    @FXML
+    private ScrollPane P_scroll;
+
+    @FXML
+    private Label labelParticipaciones;
+
+    @FXML
+    private Label labelPerfil;
+
+    @FXML
+    private ListView<?> vl_respuestas;
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
 }
