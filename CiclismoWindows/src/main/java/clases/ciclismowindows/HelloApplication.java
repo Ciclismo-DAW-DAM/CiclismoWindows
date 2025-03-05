0b085ae78982407f8d3d34980c8b239d2e2caf52
Antonioquiros <antonioquigar18@gmail.com>
2025-03-05 16:54:51 +0100

    cambios vista

--- CiclismoWindows/src/main/java/clases/ciclismowindows/HelloApplication.java
+++ CiclismoWindows/src/main/java/clases/ciclismowindows/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/clases/vistas/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Hello!");
+        stage.setTitle("Aplicacion de ciclismo");
         stage.setScene(scene);
         stage.show();
     }
--- CiclismoWindows/src/main/java/controladores/loginController.java
+++ CiclismoWindows/src/main/java/controladores/loginController.java
@@ -1,4 +1,22 @@
 package controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class loginController {
+    @FXML
+    private Button botonContrasena;
+
+    @FXML
+    private Button botonLog;
+
+    @FXML
+    private Button botonRegistrarse;
+
+    @FXML
+    private TextField contrasenaLog;
+
+    @FXML
+    private TextField correoLog;
 }
