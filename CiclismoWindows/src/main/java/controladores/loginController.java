85c874ae63ef82ff4de3645fc37be77d21ad8efb
Antonioquiros <antonioquigar18@gmail.com>
2025-03-05 18:21:28 +0100

    comprobar usuario y redirigir a otra pestaña

--- CiclismoWindows/src/main/java/controladores/loginController.java
+++ CiclismoWindows/src/main/java/controladores/loginController.java
@@ -1,9 +1,15 @@
 package controladores;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class loginController {
     @FXML
@@ -22,7 +28,38 @@ public class loginController {
     private TextField correoLog;
 
     @FXML
-    private void iniciarSesion() {}
+    private void iniciarSesion() {
+        String correo = correoLog.getText();
+        String contrasena = contrasenaLog.getText();
+        // Aqui comprobamos que los campos email y contraseña contengan informacion
+        if(correo.isEmpty() || contrasena.isEmpty()){
+            mostrarAlerta("Ciclismo", "Debe llenar todos los campos", Alert.AlertType.ERROR);
+            return;
+        }
+        if(correo.equals("Antonio") && contrasena.equals("Quiros")){
+            mostrarAlerta("Ciclismo","Has iniciado sesion correctamente", Alert.AlertType.INFORMATION);
+            try {
+                // Cargar el archivo FXML
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/clases/vistas/home.fxml"));
+                Parent root = loader.load();
+
+                // Obtener la escena actual
+                Scene scene = new Scene(root);
+
+                // Obtener el escenario (ventana) actual y cambiar la escena
+                Stage stage = (Stage) correoLog.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();  // Mostrar la nueva ventana
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                mostrarAlerta("Error", "No se pudo cargar la nueva ventana.", Alert.AlertType.ERROR);
+            }
+        }else{
+            mostrarAlerta("Ciclismo", "Correo o contraseña incorrectos", Alert.AlertType.ERROR);
+        }
+
+    }
     @FXML
     private void registrarse() {}
     @FXML
