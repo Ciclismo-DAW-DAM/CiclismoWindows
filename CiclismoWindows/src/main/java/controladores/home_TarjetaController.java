5b6a90c845ada8c02544c7e2b394c30be454f07b
Sergio <sertrujez0420@gmail.com>
2025-03-06 17:46:07 +0100

    Home(Dinitivo)

--- CiclismoWindows/src/main/java/controladores/home_TarjetaController.java
+++ CiclismoWindows/src/main/java/controladores/home_TarjetaController.java
@@ -21,13 +21,16 @@ public class home_TarjetaController {
     private Label txtDireccion;
 
     @FXML
+    private Label txtNombre;
+
+    @FXML
     public void initialize() {}
 
 
     public void cargarDatosTarjeta(Carrera carrera){
 
         if (carrera != null) {
-
+            txtNombre.setText(carrera.getNombre());
             txtDireccion.setText(carrera.getDireccion());
             txtDescripcion.setText(carrera.getDescripcion());
             Image image = new Image(carrera.getUrlImagen());
