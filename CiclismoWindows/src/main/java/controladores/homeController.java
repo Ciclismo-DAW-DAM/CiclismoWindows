e7326f82d91768d94a9c1ce76fdde29416cc0641
Sergio <sertrujez0420@gmail.com>
2025-03-06 18:30:59 +0100

    ControladorHome
    Falta llamadas a la api y reutilizar la vista para las participaciones

--- CiclismoWindows/src/main/java/controladores/homeController.java
+++ CiclismoWindows/src/main/java/controladores/homeController.java
@@ -4,28 +4,31 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
+
 import javafx.scene.Node;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.input.MouseEvent;
+
+import javafx.scene.layout.Pane;
+
 import modelos.Carrera;
 
 import java.io.IOException;
-import java.net.URL;
+
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
-import java.util.ResourceBundle;
+
 
 public class homeController {
 
     @FXML
-    private GridPane P_grid;
+    private ScrollPane P_scroll;
 
     @FXML
-    private ScrollPane P_scroll;
+    private Pane PanelPrueba;
 
     @FXML
     private TextField txt_Buscardor;
@@ -34,60 +37,244 @@ public class homeController {
     private ListView<String> vl_respuestas = new ListView<>();
 
     ObservableList<String> filtrado = FXCollections.observableArrayList();
-    private ObservableList<String> resultados = FXCollections.observableArrayList(
-            "Raton",
-            "Perro",
-            "Gato",
-            "Loro",
-            "Tortuga",
-            "Pez",
-            "Serpiente"
-    );
+    private ObservableList<String> resultados = FXCollections.observableArrayList();
 
-    private List<Carrera> carrera = Arrays.asList(
-            new Carrera(
-                    "Granada, Gran ciudad",
-                    "https://www.spain.info/export/sites/segtur/.content/imagenes/cabeceras-grandes/andalucia/alhambra-granada-20044065-istock.jpg_1014274486.jpg",
-                    "Carrera benefica"
-            ),
-            new Carrera(
-                    "Sevilla, Ciudad",
-                    "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
-                    "Carrera de cancer"
-            )
-    );
+    private List<Carrera> carreras = new ArrayList<>();
+    private  List<Carrera> carreraFiltradas = new ArrayList<>();
+
+
+
+    private void cargarCarreras(){
+
+        carreras.clear();
+
+        carreras.add(
+                new Carrera(
+                        "Granada, Gran ciudad",
+                        "https://www.spain.info/export/sites/segtur/.content/imagenes/cabeceras-grandes/andalucia/alhambra-granada-20044065-istock.jpg_1014274486.jpg",
+                        "Carrera benefica",
+                        "Carrera de granada"
+                ));
+        carreras.add(
+                new Carrera(
+                        "Huelva, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Cadíz, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+
+                new Carrera(
+                        "Jaen, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+
+        carreras.add(
+                new Carrera(
+                        "Sevilla, Ciudad",
+                        "https://losdosviajeros.com/img/sevilla/plaza-espana-noche.jpg",
+                        "Carrera de cancer",
+                        "Carrera de Sevilla"
+                )
+        );
+    }
 
 
     @FXML
     public void initialize() {
         inicializarLisener();
 
+        cargarCarreras();
+        crearTarjetas(carreras);
 
+        cargarResultadosLista(carreras);
 
-
-        cargarTarjetas();
     }
 
     private void  inicializarLisener(){
         inicializarLisenerBusqueda();
+        inicializarLisenerListaView();
     }
 
     private void inicializarLisenerBusqueda(){
 
         txt_Buscardor.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.isEmpty()) {
+
+            // Si esta vacio oculta la lista
+
+            if ((newValue.isEmpty()) || (newValue.length() < 1)) {
+
                 // Si el campo de búsqueda está vacío, ocultar el ListView
                 vl_respuestas.setVisible(false);
-            } else {
-                // Crear una lista filtrada
                 filtrado.clear();
+                carreraFiltradas.clear();
 
-                // Recorrer la lista original y buscar coincidencias
+                cargarCarreras();
+                cargarResultadosLista(carreras);
+                crearTarjetas(carreras);
+
+            } else {
+
+                filtrado.clear();
+                carreraFiltradas.clear();
+
                 for (String item : resultados) {
                     if (item.toLowerCase().contains(newValue.toLowerCase())) {
                         filtrado.add(item);
                     }
                 }
+
+                for (Carrera c : carreras){
+                    if ((c.getNombre().toLowerCase().contains(newValue.toLowerCase())) || (c.getDireccion().toLowerCase().contains(newValue.toLowerCase()))){
+                        carreraFiltradas.add(c);
+                    }
+                }
+
+                crearTarjetas(carreraFiltradas);
+
 
                 // Actualizar el ListView con los resultados filtrados
                 vl_respuestas.setItems(filtrado);
@@ -98,32 +285,42 @@ public class homeController {
         });
     }
 
-    private void selectorDeLista(){
+    private void inicializarLisenerListaView(){
+        // Código para manejar eventos del ListView
 
-        if (filtrado.isEmpty()) {
-//            cargarTarjetas(resultados);
-        } else {
-//            cargarTarjetas(filtrado);
-        }
+        vl_respuestas.setOnMouseClicked((MouseEvent event) -> {
+            // Obtener el controlador de la tarjeta
+            String carreraSeleccionada = vl_respuestas.getSelectionModel().getSelectedItem();
 
+            txt_Buscardor.setText(carreraSeleccionada);
+
+            vl_respuestas.setVisible(false);
+
+
+        });
     }
 
-
-    private void cargarTarjetas() {
+    private void crearTarjetas(List<Carrera> list) {
         // Código para cargar tarjetas en la GridPane
 
-
-
+        PanelPrueba.getChildren().clear();
         int column = 0; // Columna inicial
         int row = 0;    // Fila inicial
 
-        for (Carrera carrera : carrera) { // Iterar sobre la lista de productos
+        double tamanioPorPanel = 489;
+
+        double x = 0;
+
+        System.out.println(list.size());
+
+        for (Carrera carrera : list) { // Iterar sobre la lista de productos
             try {
                 // Cargar el archivo FXML de la tarjeta
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/clases/vistas/home_tarjeta.fxml"));
                 Node tarjeta = fxmlLoader.load();
 
-                P_grid.setMargin(tarjeta, new Insets(170, 10, 10, 10));
+                Pane tarjetaPane = (Pane) tarjeta;
+                tarjetaPane.setPrefSize(480, 349);
 
                 // Obtener el controlador de la tarjeta
                 home_TarjetaController tarjetaController = fxmlLoader.getController();
@@ -131,12 +328,26 @@ public class homeController {
                 // Pasar los datos al controlador de la tarjeta
                 tarjetaController.cargarDatosTarjeta(carrera);
 
-                // Agregar la tarjeta al GridPane
-                P_grid.add(tarjeta, column, row);
+
+                // VAmos a poner segun la tarjeta
+                if (column == 0){
+                    x = 70;
+                }
+                if (column == 1){
+                    x = 620;
+                }
+
+                double y = (tamanioPorPanel - 70) * row;
+
+                tarjetaPane.setLayoutX(x);
+                tarjetaPane.setLayoutY(y);
+
+                PanelPrueba.getChildren().add(tarjetaPane);
+                PanelPrueba.setPrefHeight((tamanioPorPanel ) * row);
 
                 // Ajustar la posición de las tarjetas
                 column++;
-                if (column == 3) { // Cambiar de fila después de 3 columnas
+                if (column == 2) { // Cambiar de fila después de 3 columnas
                     column = 0;
                     row++;
                 }
@@ -145,6 +356,16 @@ public class homeController {
             }
         }
 
+    }
+
+
+    private void cargarResultadosLista(List<Carrera> list){
+        resultados.clear();
+
+        for (Carrera c : list){
+            resultados.add(c.getNombre());
+            resultados.add(c.getDireccion());
+        }
 
 
     }
