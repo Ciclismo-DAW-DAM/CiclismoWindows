0421bc1cd84cf47e4406a910097d56be2c1ef74e
BichoPalillo <162630409+BichoPalillo@users.noreply.github.com>
2025-03-07 16:14:15 +0100

    Update HelloApplication.java

--- CiclismoWindows/src/main/java/clases/ciclismowindows/HelloApplication.java
+++ CiclismoWindows/src/main/java/clases/ciclismowindows/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/clases/vistas/detalleCarrera.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/clases/vistas/home.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root, 1300, 780);
         primaryStage.setTitle("Ciclismo");
